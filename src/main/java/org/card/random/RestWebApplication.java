315ef090219ed9a497878d9bc2a717b161d8deb3
RandySpiritPunch <aaron.beraki@gmail.com>
2016-06-23 22:56:53 -0500

    add spring dependency to allow application to run in liberty and spring boot tomcat container

--- src/main/java/org/card/random/RestWebApplication.java
+++ src/main/java/org/card/random/RestWebApplication.java
@@ -1,12 +1,25 @@
 package org.card.random;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class RestWebApplication {
+public class RestWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(restWebApplicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(restWebApplicationClass);
+    }
+
+    private static Class<RestWebApplication> restWebApplicationClass = RestWebApplication.class;
+
 }
